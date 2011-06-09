96b507a54caf63689dd5d432b475886b1156efeb
hyq.neuron@gmail.com <hyq.neuron@gmail.com@9276ac26-7d46-f2d9-6452-13fbcf2ae354>
2011-06-09 15:04:13 +0000

    first draft

---
+++ trunk/GlobalVariables.h
@@ -0,0 +1,53 @@
+//prevent multiple inclusion
+#if defined GlobalVariablesDefined 
+#else
+#define GlobalVariablesDefined yes
+
+//---code starts ---
+#include <iostream>
+#include <fstream>
+#include <list>
+#include "DataTypes.h"
+using namespace std;
+
+//All globla variables are prefixed with cs, meaning "current state"
+//-----Global variables
+
+int csLineNumber = 0;
+int csInstructionOffset;
+
+fstream csInput;
+fstream csOutput;
+char *csSource;
+int csMaxReg = 0;
+
+bool csReplaceMode;
+int csOutputSectionOffset;
+int csOutputSectionSize;
+int csOutputInstructionOffset;
+
+list<MasterParser*>		csMasterParserList;  //List of parsers to loaded at initialization
+list<LineParser*>		csLineParserList;
+list<InstructionParser*> csInstructionParserList;
+list<DirectiveParser*>	csDirectiveParserList;
+
+
+InstructionRule** csInstructionRules; //sorted array
+int* csInstructionRuleIndices; //Instruction name index of the corresponding element in csInstructionRules
+int csInstructionRuleCount;
+list<InstructionRule*>  csInstructionRulePrepList; //used for preperation
+
+MasterParser*		csMasterParser;  //curent Master Parser
+LineParser*			csLineParser;
+InstructionParser*	csInstructionParser;
+DirectiveParser*	csDirectiveParser;
+
+list<Line> csLines;
+list<Instruction> csInstructions;
+list<Directive> csDirectives;
+list<LabelRequest> csLabelRequests;
+list<Label> csLabels;
+
+//-----End of global variables
+
+#endif
